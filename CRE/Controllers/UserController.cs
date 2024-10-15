70777b641bcdb027a465d621079d5f57bf6167b4
RenzCodeRepo <renzbaladjay25@gmail.com>
2024-10-15 21:51:01 +0800

    -- fixed the roles in seed file -- still tired -- starting on the login

--- CRE/Controllers/UserController.cs
+++ CRE/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRE.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRE.Controllers
 {
@@ -8,5 +10,36 @@ namespace CRE.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                //if (result.Succeeded)
+                //{
+                //    // Check if the logged-in user has the 'Developer' role
+                //    var user = await _userManager.FindByEmailAsync(model.Email);
+                //    var roles = await _userManager.GetRolesAsync(user);
+
+                //    if (roles.Contains("Developer"))
+                //    {
+                //        // You're logged in with the developer account
+                //        return RedirectToAction("DeveloperDashboard", "Home");
+                //    }
+                //    else
+                //    {
+                //        // Non-developer login
+                //        return RedirectToAction("UserDashboard", "Home");
+                //    }
+                //}
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            return View(model);
+        }
+
+
     }
 }
